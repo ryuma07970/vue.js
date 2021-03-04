d5d38cf9f99a89cdd65f4034963cb129c81e69e1
ryuma07970 <ryuma07970@outlook.jp>
2021-03-04 17:12:33 +0900

    first commit

---
+++ vue-quest/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-app>
+    <v-toolbar 
+      dark
+      src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg"
+      max-height="60"
+    >
+      <v-toolber-title>YouTube Curation</v-toolber-title>
+      
+    </v-toolbar>
+
+    <top />
+
+    <v-toolbar
+      dark
+      src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg"
+    >
+      <v-spacer />
+      <p>@QuestAcademia</p>
+    </v-toolbar>
+  </v-app>
+</template>
+
+<script>
+  import Top from './components/Top.vue'
+
+  export default {
+    name: 'App',
+    components: {
+      Top,
+    }
+  }
+</script>
